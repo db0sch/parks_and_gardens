babb1e3680627abee929eee11e97b7539094d17e
Dimitri Bosch <dimitribosch@hotmail.com>
2020-05-13 18:09:48 +0200

    Livecode done

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,8 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  resources :gardens do # GardensController#...
+    resources :plants, only: [:create] # PlantsController#create
+  end
+  resources :plants, only: [:destroy]
 end
+
+# DELETE plants/:id
